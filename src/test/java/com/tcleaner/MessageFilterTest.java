42003413c1959273e7e42b55aa9c2c539bb1f59b
thesmithmode <117716736+thesmithmode@users.noreply.github.com>
2026-04-12 12:13:50 +0300

    FIX: MessageFilterTest - разрешить ambiguous fromParameters
    Явно кастовать null к (String) чтобы компилятор знал какую перегруженную
    версию fromParameters вызывать. Проблема появилась в коммите 0c34c2a
    где добавили перегрузку fromParameters(LocalDate, LocalDate, ...).

--- src/test/java/com/tcleaner/MessageFilterTest.java
+++ src/test/java/com/tcleaner/MessageFilterTest.java
@@ -52,7 +52,7 @@ class MessageFilterTest {
         @Test
         @DisplayName("Все параметры null или пустые → null")
         void allNullOrBlankReturnsNull() {
-            assertThat(MessageFilter.fromParameters(null, null, null, null)).isNull();
+            assertThat(MessageFilter.fromParameters((String)null, (String)null, null, null)).isNull();
             assertThat(MessageFilter.fromParameters("", "  ", "", "")).isNull();
         }
 
@@ -83,7 +83,7 @@ class MessageFilterTest {
         @Test
         @DisplayName("Корректно парсит ключевые слова (CSV)")
         void parsesKeywords() throws Exception {
-            MessageFilter filter = MessageFilter.fromParameters(null, null, " java , spring ", " spam ");
+            MessageFilter filter = MessageFilter.fromParameters((String)null, (String)null, " java , spring ", " spam ");
             assertThat(filter.matches(msg("2025-01-01T00:00:00", "java rocks"))).isTrue();
             assertThat(filter.matches(msg("2025-01-01T00:00:00", "buy spam"))).isFalse();
         }
