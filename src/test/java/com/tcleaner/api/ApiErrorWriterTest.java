000a466b354ef61dbd8e771f065a09f2fd822d68
thesmithmode <117716736+thesmithmode@users.noreply.github.com>
2026-06-05 13:23:26 +0300

    FIX: Детерминированный выбор JAR и расширение тестов ошибок API

--- src/test/java/com/tcleaner/api/ApiErrorWriterTest.java
+++ src/test/java/com/tcleaner/api/ApiErrorWriterTest.java
@@ -11,13 +11,64 @@ class ApiErrorWriterTest {
     void writesJsonWithEscapedCharacters() throws Exception {
         MockHttpServletResponse response = new MockHttpServletResponse();
 
-        ApiErrorWriter.writeJson(response, 400, "bad\"code", "line1\nline2\\tail");
+        ApiErrorWriter.writeJson(response, 400, "bad\"code", "line1\nline2\rline3\tend\\tail");
 
         assertThat(response.getStatus()).isEqualTo(400);
         assertThat(response.getCharacterEncoding()).isEqualTo("UTF-8");
         assertThat(response.getContentType()).isEqualTo("application/json;charset=UTF-8");
         assertThat(response.getContentAsString()).isEqualTo(
-            "{\"code\":\"bad\\\"code\",\"message\":\"line1\\nline2\\\\tail\"}"
+            "{\"code\":\"bad\\\"code\",\"message\":\"line1\\nline2\\rline3\\tend\\\\tail\"}"
+        );
+    }
+
+    @Test
+    void writesJsonWithNullCodeAndMessage() throws Exception {
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        ApiErrorWriter.writeJson(response, 400, null, null);
+
+        assertThat(response.getStatus()).isEqualTo(400);
+        assertThat(response.getCharacterEncoding()).isEqualTo("UTF-8");
+        assertThat(response.getContentType()).isEqualTo("application/json;charset=UTF-8");
+        assertThat(response.getContentAsString()).isEqualTo(
+            "{\"code\":null,\"message\":null}"
+        );
+    }
+
+    @Test
+    void writesJsonWithPartialNullValues() throws Exception {
+        MockHttpServletResponse responseWithNullCode = new MockHttpServletResponse();
+        MockHttpServletResponse responseWithNullMessage = new MockHttpServletResponse();
+
+        ApiErrorWriter.writeJson(responseWithNullCode, 400, null, "only-message");
+        ApiErrorWriter.writeJson(responseWithNullMessage, 400, "only-code", null);
+
+        assertThat(responseWithNullCode.getStatus()).isEqualTo(400);
+        assertThat(responseWithNullCode.getCharacterEncoding()).isEqualTo("UTF-8");
+        assertThat(responseWithNullCode.getContentType()).isEqualTo("application/json;charset=UTF-8");
+        assertThat(responseWithNullCode.getContentAsString()).isEqualTo(
+            "{\"code\":null,\"message\":\"only-message\"}"
+        );
+
+        assertThat(responseWithNullMessage.getStatus()).isEqualTo(400);
+        assertThat(responseWithNullMessage.getCharacterEncoding()).isEqualTo("UTF-8");
+        assertThat(responseWithNullMessage.getContentType()).isEqualTo("application/json;charset=UTF-8");
+        assertThat(responseWithNullMessage.getContentAsString()).isEqualTo(
+            "{\"code\":\"only-code\",\"message\":null}"
+        );
+    }
+
+    @Test
+    void writesJsonWithNonAsciiCharacters() throws Exception {
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        ApiErrorWriter.writeJson(response, 500, "ошибка", "emoji 🤖 and accented é");
+
+        assertThat(response.getStatus()).isEqualTo(500);
+        assertThat(response.getCharacterEncoding()).isEqualTo("UTF-8");
+        assertThat(response.getContentType()).isEqualTo("application/json;charset=UTF-8");
+        assertThat(response.getContentAsString()).isEqualTo(
+            "{\"code\":\"ошибка\",\"message\":\"emoji 🤖 and accented é\"}"
         );
     }
 }
